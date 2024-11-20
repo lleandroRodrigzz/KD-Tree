d8130e92d57a0539c0fc88b4ae97eb3b4e84316d
lleandroRodrigzz <leandromarcoscasse@gmail.com>
2024-11-20 16:34:32 -0300

    KD-Tree Finalizado

---
+++ KDTree.h
@@ -0,0 +1,185 @@
+#include <conio.h>
+#include <conio2.h>
+
+#define K 2		//Define a dimensão dos dados
+#define N 15	//Define o numero de pontos
+
+struct kdtree{
+	int ponto[K];
+	struct kdtree *esq, *dir;
+};
+typedef struct kdtree KDT;
+
+/*----------------------- Cordenadas ------------------------*/
+int geradorNum()
+{
+	return rand() % 70 + 10;	
+}
+
+void geradorCordenadas(int pontos[N][K])
+{
+	int i,j;
+	
+	for(i = 0; i < N; i++)
+	{
+		for(j = 0; j < K; j++)
+		{
+			pontos[i][j] = geradorNum();
+		}
+	}
+}
+
+void ordenarPontosNivel(int pontos[N][K], int inicio, int fim, int discri)
+{
+	int posMenor, aux, i, j, l;
+	fim++;
+	for(i = inicio; i < fim - 1; i++)
+	{
+		posMenor = i;
+		for(j = i; j < fim; j++)
+		{
+			if(pontos[j][discri] < pontos[posMenor][discri])
+				posMenor = j;
+		}
+	}
+	// Trocar todas as posicoes menor que i
+	for(l = 0; l < K; l++)
+	{
+		aux = pontos[i][l];
+		pontos[i][l] = pontos[posMenor][l];
+        pontos[posMenor][l] = aux;
+	}
+}
+
+void exibeCordenadas(int pontos[N][K])
+{
+	for (int i = 0; i < N; i++)
+        printf("\t-(%d, %d)-\n", pontos[i][0], pontos[i][1]);
+    printf("\n\n");
+}
+/*-----------------------------------------------------------*/
+
+/*----------------------- Calculo ------------------------*/
+float distanciaEuclidiana(int ponto[], int ponto1[])
+{
+	int i;
+	double dist = 0.0;
+	
+	for(i = 0; i < K; i++)
+	{
+		dist += pow(ponto[i] - ponto[i], 2);
+	}
+	return sqrt(dist);
+}
+/*--------------------------------------------------------*/
+
+/*----------------------- KD-Tree ------------------------*/
+KDT *criaNo(int ponto[])
+{
+	int i;
+	KDT *no = (KDT*)malloc(sizeof(KDT));
+	for(i = 0; i < K; i++)
+		no->ponto[i] = ponto[i];
+	no->esq = no->dir = NULL;
+	
+	return no;
+}
+
+void construirKDT(KDT* *Raiz, int pontos[N][K], int inicio, int fim, int nivel)
+{
+	int discri, meio;
+	if(inicio <= fim)
+	{
+		discri = nivel % K;
+		ordenarPontosNivel(pontos, inicio, fim, discri);
+		
+		meio = (inicio + fim) / 2;
+		
+		*Raiz = criaNo(pontos[meio]);
+		
+		construirKDT(&(*Raiz)->esq, pontos, inicio, meio - 1, nivel + 1);
+		construirKDT(&(*Raiz)->dir, pontos, meio + 1, fim, nivel + 1);
+	}
+}
+
+void insereKDT(KDT* *Raiz, int ponto[], int nivel)
+{
+	int discri;
+	discri = N % K;
+	if(ponto[discri] < (*Raiz)->ponto[discri])
+	{
+		insereKDT(&(*Raiz)->esq, ponto, nivel + 1);
+	}
+	else
+	{
+		insereKDT(&(*Raiz)->dir, ponto, nivel + 1);
+	}
+}
+
+void buscaKDT(KDT *Raiz, int ponto[], int raio, char *flag)
+{
+	static int cont = 0;
+	double dEuclid;
+	if(Raiz != NULL)
+	{
+		dEuclid = distanciaEuclidiana(Raiz->ponto, ponto);
+		if(dEuclid <= raio)
+		{
+			cont++;
+			printf("[%d,%d],  ", Raiz->ponto[0], Raiz->ponto[1]);
+			if(cont == 9)
+			{
+				printf("\n\t\t\t");
+				cont = 0;
+			}
+			*flag = 1;
+		}
+		buscaKDT(Raiz->esq, ponto, raio, flag);
+		buscaKDT(Raiz->dir, ponto, raio, flag);
+	}
+}
+
+void exibeKDT(KDT *Raiz)
+{
+	static int nivel = -1, i;
+	if(Raiz != NULL)
+	{
+		nivel++;
+		exibeKDT(Raiz->dir);
+		
+		for(i = 0; i < 5; i++)
+			printf(" ");
+		printf("(%d,%d)\n", Raiz->ponto[0], Raiz->ponto[1]);
+		
+		exibeKDT(Raiz->esq);
+		nivel--;
+	}
+	
+}
+
+void exibeKDTVert(KDT *raiz, int x, int y, int deslocamento)
+{
+	int novoXEsq, novoXDir, novoY;
+    if (raiz != NULL) {
+        gotoxy(x, y);
+        // Calcula a posição dos filhos
+        printf("(%d,%d)\n", raiz->ponto[0], raiz->ponto[1]);
+        novoXEsq = x - deslocamento;
+        novoXDir = x + deslocamento;
+        novoY = y + 2;
+        // Exibe as subárvores esquerda e direita com recuo
+        exibeKDTVert(raiz->esq, novoXEsq, novoY, deslocamento / 2);
+        exibeKDTVert(raiz->dir, novoXDir, novoY, deslocamento / 2);
+    }
+}
+
+void limpaKDT(KDT *Raiz)
+{
+	if(Raiz != NULL)
+	{
+		limpaKDT(Raiz->esq);
+		limpaKDT(Raiz->dir);
+		free(Raiz);
+	}
+}
+/*--------------------------------------------------------*/
---
+++ main.cpp
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <windows.h>
+#include "KDTree.h"
+
+void configurarConsole(int largura, int altura) 
+{
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    SMALL_RECT janela = {0, 0, largura - 1, altura - 1};
+    COORD tamanhoBuffer = {largura, altura};
+
+    // Ajusta o tamanho do buffer
+    SetConsoleScreenBufferSize(hOut, tamanhoBuffer);
+
+    // Ajusta o tamanho da janela
+    SetConsoleWindowInfo(hOut, TRUE, &janela);
+
+    // Maximiza o console
+    HWND console = GetConsoleWindow();
+    ShowWindow(console, SW_MAXIMIZE);
+}
+
+void preencherTelaAzul() {
+    int linhas = 30; // Máximo de linhas para o console
+    int colunas = 120; // Máximo de colunas para o console
+    int i, j;
+
+    textbackground(BLUE); 
+    clrscr(); // Limpa a tela com a cor de fundo configurada
+
+    for (i = 1; i <= linhas; i++) {
+        for (j = 1; j <= colunas; j++) {
+            gotoxy(j, i); 
+            printf(" "); 
+        }
+    }
+    gotoxy(120,30);
+}
+
+void Moldura(int CI, int LI, int CF, int LF, int CorT, int CorF) 
+{
+    int i;
+    textcolor(CorT);
+    textbackground(CorF);
+    gotoxy(CI, LI); printf("%c", 201);
+    gotoxy(CI, LF); printf("%c", 200);
+    gotoxy(CF, LI); printf("%c", 187);
+    gotoxy(CF, LF); printf("%c", 188);
+
+    for (i = CI + 1; i < CF; i++) {
+        gotoxy(i, LI); printf("%c", 205);
+        gotoxy(i, LF); printf("%c", 205);
+    }
+    for (i = LI + 1; i < LF; i++) {
+        gotoxy(CI, i); printf("%c", 186);
+        gotoxy(CF, i); printf("%c", 186);
+    }
+    textcolor(7);
+    textbackground(0);
+}
+
+int main()
+{
+	configurarConsole(120,30);
+	preencherTelaAzul();
+	textbackground(BLUE); 
+	textcolor(YELLOW);
+	
+	int pontos[N][K], pontoBusca[K], raio;
+    KDT *Raiz = NULL;
+    char flag = 0;
+    srand(time(NULL));
+	
+	/*------------------------------------------ PRIMEIRA TELA ----------------------------------------------------------*/
+	system("cls");
+	Moldura(3,2,118,4,BLACK,BLUE); //Borda Titulo
+	textbackground(BLUE);
+	textcolor(YELLOW);
+	gotoxy(12,3); printf("#### ARVORE KD-TREE - DESENVOLVIDO POR LEANDRO M. C. RODRIGUES - PROFESSOR FRANCISCO DE ASSIS ####");
+	
+	Moldura(54,4,66,6,BLACK,BLUE); //Borda SubTitulo
+	textbackground(BLUE);
+	textcolor(YELLOW);
+	gotoxy(57,5); printf("KD-TREE");
+    
+    geradorCordenadas(pontos);
+    gotoxy(24,6);printf("*Numero De Pontos (N): %d*", N);
+    gotoxy(71,6);printf("*Dimensao Dos Dados (K): %d*", K);
+    gotoxy(4,10);printf("*Coordenadas Geradas*\n");
+    exibeCordenadas(pontos);
+    getch();
+    /*-------------------------------------------------------------------------------------------------------------------*/
+    
+    /*------------------------------------------ SEGUNDA TELA ----------------------------------------------------------*/
+    system("cls");
+    
+    Moldura(3,2,118,4,BLACK,BLUE); //Borda Titulo
+	textbackground(BLUE);
+	textcolor(YELLOW);
+	gotoxy(12,3); printf("#### ARVORE KD-TREE - DESENVOLVIDO POR LEANDRO M. C. RODRIGUES - PROFESSOR FRANCISCO DE ASSIS ####");
+	
+	Moldura(54,4,66,6,BLACK,BLUE); //Borda SubTitulo
+	textbackground(BLUE);
+	textcolor(YELLOW);
+	gotoxy(57,5); printf("KD-TREE");
+	
+    construirKDT(&Raiz, pontos, 0, N - 1, 0);
+    gotoxy(40,13);printf("KD-Tree (Vertical):");
+    //exibeKDT(Raiz);
+    exibeKDTVert(Raiz, 46, 15, 22);
+    
+    pontoBusca[0] = geradorNum();
+    pontoBusca[1] = geradorNum();
+    raio = rand() % 10 + 10;
+	
+	gotoxy(16,6);printf("*Ponto para Buscar: (%d, %d)*", pontoBusca[0], pontoBusca[1]);
+    gotoxy(78,6);printf("*Raio: %d*", raio);
+	
+	printf("\n\n\n\t\t*Resultado: ");
+	buscaKDT(Raiz, pontoBusca, raio, &flag);
+	if(!flag)
+	{
+		printf("Nao Foram Encontrados Pontos !");
+	} 
+	limpaKDT(Raiz);
+    getch();
+    /*------------------------------------------------------------------------------------------------------------------*/
+    
+    textbackground(BLACK);
+	system("cls");
+	gotoxy(50,15);
+    return 0;
+}
